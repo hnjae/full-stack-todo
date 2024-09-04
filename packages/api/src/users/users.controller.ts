809dfffb1c8992ca9761d33a22071e8531a7715e
KIM Hyunjae <42675338+hnjae@users.noreply.github.com>
2024-09-04 19:58:45 +0900

    feat(api): add user create api

--- packages/api/src/users/users.controller.ts
+++ packages/api/src/users/users.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { ApiOperation } from '@nestjs/swagger';
+
+import { CreateUserDto } from './users.dto';
+import { UsersService } from './users.service';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post()
+  @ApiOperation({ summary: 'Create user' })
+  async create(@Body() createUserDto: CreateUserDto) {
+    return this.usersService.create(createUserDto);
+  }
+}
--- packages/api/src/users/users.module.ts
+++ packages/api/src/users/users.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 
 @Module({
-  providers: [UsersService],
+  providers: [UsersService, PrismaService],
   controllers: [UsersController],
 })
 export class UsersModule {}
