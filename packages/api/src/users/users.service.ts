67a987b5a00867daa30dd72c9eee11abcfdcdb3f
KIM Hyunjae <hnjae@outlook.com>
2024-09-04 19:58:45 +0900

    feat(api): add user create api

--- packages/api/src/users/users.service.ts
+++ packages/api/src/users/users.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+import { CreateUserDto } from './users.dto';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(private prisma: PrismaService) {}
+
+  async create(userDto: CreateUserDto) {
+    const { email } = userDto;
+    const hashedPassword = await bcrypt.hash(userDto.password, 10);
+    const user = await this.prisma.user.create({
+      data: { email: email, password: hashedPassword },
+    });
+
+    // TODO: remove hashed password from this <2024-09-04>
+    return user;
+  }
+}
