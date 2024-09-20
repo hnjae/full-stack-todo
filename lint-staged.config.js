0fdadb1c0ff0b1f2350bfecbc7d519209fddbc4c
KIM Hyunjae <hnjae@outlook.com>
2024-09-20 20:59:55 +0900

    ci: add yaml file formatting to lint-staged

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,4 +1,4 @@
 export default {
   '*.{js,mjs,cjs,jsx,mjsx,ts,tsx,mtsx}': ['eslint --fix', 'prettier --write'],
-  '*.{css,html,json,md}': 'prettier --ignore-unknown --write',
+  '*.{css,html,json,yaml,md}': 'prettier --ignore-unknown --write',
 };
