48efc682aa4c0187de1b371d1cb1ab1822ede4de
KIM Hyunjae <42675338+hnjae@users.noreply.github.com>
2024-09-20 20:31:21 +0900

    test(api): add few end-to-end tests for users module

---
+++ packages/api/test/users.e2e-spec.ts
@@ -0,0 +1,87 @@
+import { INestApplication } from '@nestjs/common';
+import { Test } from '@nestjs/testing';
+import { mockDeep } from 'jest-mock-extended';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { UsersModule } from 'src/users/users.module';
+import * as request from 'supertest';
+
+describe('Users', () => {
+  let app: INestApplication;
+
+  const mockPrismaService = mockDeep<PrismaService>();
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [UsersModule],
+    })
+      .overrideProvider(PrismaService)
+      .useValue(mockPrismaService)
+      .compile();
+
+    app = moduleRef.createNestApplication();
+    await app.init();
+  });
+
+  it(`/GET all`, () => {
+    const users = [
+      {
+        id: 'bb70a9a3-6eb8-45c9-b006-ca085517bcf8',
+        email: 'example@example.com',
+        password: 'hashed-password',
+      },
+      {
+        id: '35d078a5-48aa-4a37-ad7c-ab7de27d8a35',
+        email: 'example2@example2.com',
+        password: 'hashed-password2',
+      },
+    ];
+    mockPrismaService.user.findMany.mockResolvedValue(users);
+
+    return request(app.getHttpServer())
+      .get('/users/all')
+      .expect(200)
+      .expect(users);
+  });
+
+  it(`/POST create user`, async () => {
+    const user = {
+      email: 'example@example.com',
+      password: 'unhashed-password',
+    };
+
+    mockPrismaService.user.create.mockResolvedValue({
+      id: 'ce392f27-1746-474b-ba18-20f9b6b9d09b',
+      ...user,
+    });
+
+    const response = await request(app.getHttpServer())
+      .post('/users')
+      .send(user)
+      .set('Accept', 'application/json')
+      .expect(201);
+
+    expect(response.body.email).toEqual(user.email);
+
+    return response;
+  });
+
+  // WIP
+  // it(`/POST same email as existing user`, async () => {
+  //   const user = {
+  //     email: 'example2@example.com',
+  //     password: 'unhashed-password',
+  //   };
+  //
+  //   const response = await request(app.getHttpServer())
+  //     .post('/users')
+  //     .send(user)
+  //     .set('Accept', 'application/json')
+  //     .expect(409);
+  //
+  //   expect(response.body.email).toEqual(user.email);
+  // });
+
+  afterAll(async () => {
+    await app.close();
+  });
+});
