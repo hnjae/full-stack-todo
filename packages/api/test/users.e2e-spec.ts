5690e1dce902eb66a3a674f03baf66f04346e33c
KIM Hyunjae <42675338+hnjae@users.noreply.github.com>
2024-09-23 16:29:15 +0900

    test(api): update GET /users/all test

--- packages/api/test/users.e2e-spec.ts
+++ packages/api/test/users.e2e-spec.ts
@@ -38,12 +38,26 @@ describe('Users', () => {
         createdAt: new Date('2024-09-23T15:28:04Z'),
       },
     ];
+    const expectedReturns = [
+      {
+        id: 'bb70a9a3-6eb8-45c9-b006-ca085517bcf8',
+        email: 'example@example.com',
+        password: 'hashed-password',
+        createdAt: '2024-09-23T15:27:53.000Z',
+      },
+      {
+        id: '35d078a5-48aa-4a37-ad7c-ab7de27d8a35',
+        email: 'example2@example2.com',
+        password: 'hashed-password2',
+        createdAt: '2024-09-23T15:28:04.000Z',
+      },
+    ];
     mockPrismaService.user.findMany.mockResolvedValueOnce(users);
 
     return request(app.getHttpServer())
       .get('/users/all')
       .expect(200)
-      .expect(users);
+      .expect(expectedReturns);
   });
 
   it(`/POST create user`, async () => {
