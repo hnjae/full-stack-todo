d439f5dd3aad4527e862530f884c5ae7c119145f
KIM Hyunjae <hnjae@outlook.com>
2024-10-31 11:14:27 +0900

    refactor(api): handle HttpException within controller layer

--- packages/api/src/users/users.controller.ts
+++ packages/api/src/users/users.controller.ts
@@ -3,11 +3,14 @@ import {
   Controller,
   Delete,
   Get,
+  HttpException,
+  HttpStatus,
   Param,
   Post,
   Put,
 } from '@nestjs/common';
 import { ApiOperation } from '@nestjs/swagger';
+import { Prisma } from '@prisma/client';
 
 import { CreateUserDto, UpdateUserDto } from './users.dto';
 import { UsersService } from './users.service';
@@ -24,13 +27,30 @@ export class UsersController {
   @Post()
   @ApiOperation({ summary: 'Create user' })
   async create(@Body() createUserDto: CreateUserDto) {
-    return this.usersService.create(createUserDto);
+    try {
+      const user = await this.usersService.create(createUserDto);
+
+      return user;
+    } catch (error) {
+      if (error instanceof Prisma.PrismaClientKnownRequestError) {
+        if (error.code == 'P2002') {
+          // "Unique constraint failed on the {constraint}"
+          throw new HttpException('User already exists', HttpStatus.CONFLICT);
+        }
+      }
+
+      throw error;
+    }
   }
 
   @Get(':email')
   @ApiOperation({ summary: 'Get user data' })
   async get(@Param('email') email: string) {
-    return this.usersService.get(email);
+    const user = await this.usersService.get(email);
+    if (user == null) {
+      throw new HttpException('User does not exists', HttpStatus.NOT_FOUND);
+    }
+    return user;
   }
 
   @Put(':email')
@@ -39,12 +59,38 @@ export class UsersController {
     @Param('email') email: string,
     @Body() updateUserDto: UpdateUserDto,
   ) {
-    return this.usersService.update(email, updateUserDto);
+    try {
+      const user = await this.usersService.update(email, updateUserDto);
+      return user;
+    } catch (error) {
+      if (error instanceof Prisma.PrismaClientKnownRequestError) {
+        if (error.code == 'P2025') {
+          // Record to update not found.
+          throw new HttpException('User does not exists', HttpStatus.NOT_FOUND);
+        } else if (error.code == 'P2002') {
+          // Unique constraint failed on the fields:
+          throw new HttpException('User already exists', HttpStatus.CONFLICT);
+        }
+      }
+
+      throw error;
+    }
   }
 
   @Delete(':email')
   @ApiOperation({ summary: 'Delete user' })
   async delete(@Param('email') email: string) {
-    return this.usersService.delete(email);
+    try {
+      const user = await this.usersService.delete(email);
+      return user;
+    } catch (error) {
+      if (error instanceof Prisma.PrismaClientKnownRequestError) {
+        if (error.code == 'P2025') {
+          // Record to delete does not exist.
+          throw new HttpException('User does not exists', HttpStatus.NOT_FOUND);
+        }
+      }
+      throw error;
+    }
   }
 }
