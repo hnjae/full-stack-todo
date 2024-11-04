5b1807b527dd4b647a286a7cec9f70b3aca03b99
KIM Hyunjae <hnjae@outlook.com>
2024-11-04 16:48:39 +0900

    test(auth): add e2e tests for the /auth/login endpoint

--- packages/api/test/auth.e2e-spec.ts
+++ packages/api/test/auth.e2e-spec.ts
@@ -1,10 +1,12 @@
 import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { Test } from '@nestjs/testing';
 import { Prisma } from '@prisma/client';
+import * as bcrypt from 'bcrypt';
 import { mockDeep } from 'jest-mock-extended';
+import * as jwt from 'jsonwebtoken';
 import { AuthModule } from 'src/auth/auth.module';
 import { PrismaService } from 'src/prisma/prisma.service';
-import { CreateUserDto } from 'src/users/users.dto';
+import { CreateUserDto, LoginUserDto } from 'src/users/users.dto';
 import * as request from 'supertest';
 
 describe('auth', () => {
@@ -81,5 +83,73 @@ describe('auth', () => {
       .send(user)
       .set('Accept', 'application/json')
       .expect(400);
+  });
+
+  it(`/POST /auth/login success`, async () => {
+    const userDto: LoginUserDto = {
+      email: 'example@example.com',
+      password: 'unhashed-password',
+    };
+
+    mockPrismaService.user.findUnique.mockResolvedValueOnce({
+      id: '7f714f74-436a-45ae-ab27-53ca3ab558de',
+      createdAt: new Date('2024-07-21T16:38:29Z'),
+      ...userDto,
+      password: await bcrypt.hash(userDto.password, 10),
+    });
+
+    const response = await request(app.getHttpServer())
+      .post('/auth/login')
+      .send(userDto)
+      .set('Accept', 'application/json')
+      .expect(200);
+
+    expect(response.body).toHaveProperty('accessToken');
+    const token = jwt.decode(response.body.accessToken);
+
+    expect(token).toHaveProperty('sub');
+    expect(token).toHaveProperty('iat');
+    expect(token).toHaveProperty('exp');
+
+    return response;
+  });
+
+  it(`/POST /auth/login fail (password-unmatch)`, async () => {
+    const userDto: LoginUserDto = {
+      email: 'example@example.com',
+      password: 'unhashed-password',
+    };
+
+    mockPrismaService.user.findUnique.mockResolvedValueOnce({
+      id: '9fbcc95b-48e2-49de-a64c-fcbe294d7c4c',
+      createdAt: new Date('2014-07-21T16:38:29Z'),
+      ...userDto,
+      password: await bcrypt.hash('unmatch-password', 10),
+    });
+
+    const response = await request(app.getHttpServer())
+      .post('/auth/login')
+      .send(userDto)
+      .set('Accept', 'application/json')
+      .expect(401);
+
+    return response;
+  });
+
+  it(`/POST /auth/login fail (does not exist)`, async () => {
+    const userDto: LoginUserDto = {
+      email: 'example@example.com',
+      password: 'unhashed-password',
+    };
+
+    mockPrismaService.user.findUnique.mockResolvedValueOnce(null);
+
+    const response = await request(app.getHttpServer())
+      .post('/auth/login')
+      .send(userDto)
+      .set('Accept', 'application/json')
+      .expect(401);
+
+    return response;
   });
 });
