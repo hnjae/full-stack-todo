0a5e80e9a76f9d391feee5406b56d36260c620df
KIM Hyunjae <42675338+hnjae@users.noreply.github.com>
2024-11-07 13:49:26 +0900

    feat(api): read and validate JWT secret from environment variable

--- packages/api/src/app.module.ts
+++ packages/api/src/app.module.ts
@@ -1,4 +1,6 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { z } from 'zod';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -6,7 +8,21 @@ import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [UsersModule, AuthModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      ignoreEnvFile: true,
+      validate: (config) => {
+        const configSchema = z.object({
+          JWT_SECRET: z.string().min(1),
+        });
+        return configSchema.parse(config);
+      },
+      // envFilePath: '.env.production',
+    }),
+    UsersModule,
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- packages/api/src/auth/auth.module.ts
+++ packages/api/src/auth/auth.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { JwtModule } from '@nestjs/jwt';
 import { UsersModule } from 'src/users/users.module';
 
@@ -11,9 +12,8 @@ import { AuthService } from './auth.service';
     JwtModule.register({
       // NOTE: `global` means that we don't need to import the JwtModule anywhere else in our application.
       global: true,
-      // TODO: read secret from environment-variable <2024-11-04>
-      secret:
-        'DO NOT USE THIS VALUE. INSTEAD, CREATE A COMPLEX SECRET AND KEEP IT SAFE OUTSIDE OF THE SOURCE CODE.',
+      // NOTE: 환경변수를 검증하고 사용하기 위해  process.env.JWT_SECRET 에서 읽어 들이지 않음.
+      secret: new ConfigService().get('JWT_SECRET'),
       signOptions: { expiresIn: '60s' },
     }),
   ],
