6daf48e141592a0ed4a94407612623c86bb3ceed
KIM Hyunjae <hnjae@outlook.com>
2024-12-26 20:06:06 +0900

    feat(signup): migrate from react-hook-form to antd form

--- packages/ui/src/pages/signup.tsx
+++ packages/ui/src/pages/signup.tsx
@@ -1,4 +1,4 @@
-import { SubmitHandler, useForm } from 'react-hook-form';
+import { Button, Card, Form, Input } from 'antd';
 import getApiUrl from 'src/shared/getApiUrl';
 
 // TODO: 이메일 인증? <2024-12-23>
@@ -7,6 +7,7 @@ import getApiUrl from 'src/shared/getApiUrl';
 // TODO: 패스워드 규칙을 뛰우고, 현재 입력이 해당 규칙에 맞는지 노출하기 <2024-12-23>
 // TODO: 실시간 이메일 규격 validate <2024-12-23>
 // TODO: error-handling <2024-12-23>
+// TODO: card 를 적당히 화면 중앙에 뛰우기 <2024-12-24>
 
 interface FormData {
   email: string;
@@ -14,14 +15,7 @@ interface FormData {
 }
 
 export default function SignupPage() {
-  const {
-    register,
-    handleSubmit,
-    // watch,
-    // formState: { errors },
-  } = useForm<FormData>();
-
-  const onSubmit: SubmitHandler<FormData> = async function (data) {
+  const onFinish = async function (data: FormData) {
     const apiUrl = getApiUrl();
     try {
       const response = await fetch(`${apiUrl}/auth/register`, {
@@ -42,39 +36,39 @@ export default function SignupPage() {
   };
 
   return (
-    <div className="hero bg-base-200 min-h-screen">
-      <div className="hero-content flex-col lg:flex-row-reverse">
-        <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-          <form className="card-body" onSubmit={handleSubmit(onSubmit)}>
-            <div className="text-xl">Sign up</div>
-            <div className="form-control">
-              <label className="input input-bordered flex items-center gap-2">
-                <span className="material-icons-outlined">email</span>
-                <input
-                  type="text"
-                  className="grow"
-                  placeholder="example@example.org"
-                  {...register('email', { required: true })}
-                />
-              </label>
-            </div>
-            <div className="form-control">
-              <label className="input input-bordered flex items-center gap-2">
-                <span className="material-icons-outlined">password</span>
-                <input
-                  type="password"
-                  className="grow"
-                  placeholder="password"
-                  {...register('password', { required: true })}
-                />
-              </label>
-            </div>
-            <div className="form-control mt-6">
-              <button className="btn btn-primary">Submit</button>
-            </div>
-          </form>
-        </div>
-      </div>
-    </div>
+    <Card title="Sign up" className="">
+      <Form
+        name="Sign up"
+        labelCol={{ span: 8 }}
+        wrapperCol={{ span: 16 }}
+        style={{ maxWidth: 400 }}
+        initialValues={{ remember: true }}
+        onFinish={onFinish}
+        // onFinishFailed={onFinishFailed}
+        autoComplete="off"
+      >
+        <Form.Item<FormData>
+          label="Email"
+          name="email"
+          rules={[{ required: true, message: '' }]}
+        >
+          <Input placeholder="example@example.org" />
+        </Form.Item>
+
+        <Form.Item<FormData>
+          label="Password"
+          name="password"
+          rules={[{ required: true, message: 'Please input your password!' }]}
+        >
+          <Input.Password placeholder="input password" />
+        </Form.Item>
+
+        <Form.Item label={null}>
+          <Button type="primary" htmlType="submit">
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
+    </Card>
   );
 }
