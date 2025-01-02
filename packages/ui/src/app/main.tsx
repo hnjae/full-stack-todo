7e93c6c6a6276e58e621cc007bb438d15a6ef6c9
KIM Hyunjae <42675338+hnjae@users.noreply.github.com>
2025-01-02 18:11:40 +0900

    feat(ui): add navigation menu to the root component

--- packages/ui/src/app/main.tsx
+++ packages/ui/src/app/main.tsx
@@ -1,15 +1,17 @@
 import './index.css';
 import '@fontsource/material-icons-outlined';
 
+import { HomeOutlined, LoginOutlined } from '@ant-design/icons';
 import {
   createRootRoute,
   createRoute,
   createRouter,
-  Link,
   Outlet,
   RouterProvider,
+  useNavigate,
 } from '@tanstack/react-router';
 import { TanStackRouterDevtools } from '@tanstack/router-devtools';
+import { Layout, Menu, MenuProps, theme } from 'antd';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import LoginPage from 'src/pages/login';
@@ -18,10 +20,20 @@ import signupPage from 'src/pages/signup';
 const rootRoute = createRootRoute({
   component: RootComponent,
   notFoundComponent: () => {
+    const {
+      token: { colorBgContainer, borderRadiusLG },
+    } = theme.useToken();
+
     return (
-      <div>
-        <p>This is the notFoundComponent configured on root route</p>
-        <Link to="/">Start Over</Link>
+      <div
+        style={{
+          background: colorBgContainer,
+          minHeight: 280,
+          padding: 24,
+          borderRadius: borderRadiusLG,
+        }}
+      >
+        <span className="material-icons-outlined">home</span> hello
       </div>
     );
   },
@@ -39,14 +51,64 @@ const signupRoute = createRoute({
   component: signupPage,
 });
 
+const { Header } = Layout;
+
 function RootComponent() {
+  const navigate = useNavigate();
+  const items: MenuProps['items'] = [
+    {
+      key: 'home',
+      label: 'Home',
+      icon: <HomeOutlined />,
+      onClick: () => {
+        navigate({
+          to: '/',
+        });
+      },
+    },
+    {
+      key: 'login',
+      label: 'Login',
+      icon: <LoginOutlined />,
+      onClick: () => {
+        navigate({
+          to: '/login',
+        });
+      },
+    },
+  ];
+
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">tailwindcss test</h1>
-      <span className="material-icons-outlined">home</span>
+    <Layout>
+      <Header
+        className="foo"
+        style={{
+          height: 32,
+          padding: '0 0px',
+          lineHeight: '32px',
+          display: 'flex',
+          alignItems: 'center',
+        }}
+      >
+        <Menu
+          theme="light"
+          mode="horizontal"
+          items={items}
+          defaultSelectedKeys={['home']}
+          style={{
+            height: 32,
+            padding: '0 16px',
+            lineHeight: '32px',
+            justifyContent: 'flex-end',
+            flex: 1,
+            minWidth: 0,
+          }}
+        />
+      </Header>
+
       <Outlet />
       <TanStackRouterDevtools position="bottom-right" />
-    </>
+    </Layout>
   );
 }
 
