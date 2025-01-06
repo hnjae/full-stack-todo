f2b669074ccf9ad7aa0e25affb83815fcbfd13f0
KIM Hyunjae <42675338+hnjae@users.noreply.github.com>
2025-01-06 19:05:35 +0900

    feat(MainHeader): separate left and right menu items
    Update the MainHeader component to accept separate props for
    left and right menu items, allowing for more flexible layout.

--- packages/ui/src/shared/ui/MainHeader.tsx
+++ packages/ui/src/shared/ui/MainHeader.tsx
@@ -5,10 +5,11 @@ const { Header } = Layout;
 type ItemType = Required<MenuProps>['items'][number];
 
 interface MainHeaderProps {
-  items: ItemType[];
+  leftItems?: ItemType[];
+  rightItems?: ItemType[];
 }
 
-export default function MainHeader({ items }: MainHeaderProps) {
+export default function MainHeader({ leftItems, rightItems }: MainHeaderProps) {
   return (
     <Header
       className="foo"
@@ -20,19 +21,37 @@ export default function MainHeader({ items }: MainHeaderProps) {
         alignItems: 'center',
       }}
     >
-      <Menu
-        theme="light"
-        mode="horizontal"
-        items={items}
-        style={{
-          height: 32,
-          padding: '0 16px',
-          lineHeight: '32px',
-          justifyContent: 'flex-end',
-          flex: 1,
-          minWidth: 0,
-        }}
-      />
+      {leftItems != null && (
+        <Menu
+          theme="light"
+          mode="horizontal"
+          items={leftItems}
+          style={{
+            height: 32,
+            padding: '0 16px',
+            lineHeight: '32px',
+            justifyContent: 'flex-start',
+            flex: 1,
+            minWidth: 0,
+          }}
+        />
+      )}
+
+      {rightItems != null && (
+        <Menu
+          theme="light"
+          mode="horizontal"
+          items={rightItems}
+          style={{
+            height: 32,
+            padding: '0 16px',
+            lineHeight: '32px',
+            justifyContent: 'flex-end',
+            flex: 1,
+            minWidth: 0,
+          }}
+        />
+      )}
     </Header>
   );
 }
