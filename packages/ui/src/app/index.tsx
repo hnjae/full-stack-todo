03aa262571e7315f8c4a7e10fbf5a288931d807d
KIM Hyunjae <42675338+hnjae@users.noreply.github.com>
2025-01-15 15:57:45 +0900

    refactor(ui): modularize routing logic into separate file

--- packages/ui/src/app/index.tsx
+++ packages/ui/src/app/index.tsx
@@ -1,54 +1,11 @@
 import './style.css';
 import '@fontsource/material-icons-outlined';
 
-import {
-  createRootRoute,
-  createRoute,
-  createRouter,
-  Outlet,
-  RouterProvider,
-} from '@tanstack/react-router';
-import { TanStackRouterDevtools } from '@tanstack/router-devtools';
+import { RouterProvider } from '@tanstack/react-router';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import HomePage from 'src/pages/home';
-import LoginPage from 'src/pages/login';
-import signupPage from 'src/pages/signup';
 
-const rootRoute = createRootRoute({
-  component: RootComponent,
-  notFoundComponent: HomePage,
-});
-
-const loginRoute = createRoute({
-  getParentRoute: () => rootRoute,
-  path: 'login',
-  component: LoginPage,
-});
-
-const signupRoute = createRoute({
-  getParentRoute: () => rootRoute,
-  path: 'signup',
-  component: signupPage,
-});
-
-function RootComponent() {
-  return (
-    <div>
-      <Outlet />
-      <TanStackRouterDevtools position="bottom-right" />
-    </div>
-  );
-}
-
-const routeTree = rootRoute.addChildren([signupRoute, loginRoute]);
-
-// Set up a Router instance
-const router = createRouter({
-  routeTree,
-  defaultPreload: 'intent',
-  defaultStaleTime: 5000,
-});
+import router from './router';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
