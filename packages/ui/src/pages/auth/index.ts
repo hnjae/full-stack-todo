5aeaa1f3aa95421ad48e5235a572e1d7e7f385f0
KIM Hyunjae <42675338+hnjae@users.noreply.github.com>
2025-01-15 19:13:43 +0900

    refactor(auth): refactor login page structure by separating components
    This change introduces the following updates:
    - Rename `login.tsx` to `LoginPage.tsx` for consistency.
    - Add `AuthForm.tsx`
    - Add `AuthPageLayout.tsx`
    - Add `FormData.ts` interface

--- packages/ui/src/pages/auth/index.ts
+++ packages/ui/src/pages/auth/index.ts
@@ -1,2 +1,2 @@
-export { default as LoginPage } from './ui/login.tsx';
+export { default as LoginPage } from './ui/LoginPage.tsx';
 export { default as SignupPage } from './ui/signup.tsx';
---
+++ packages/ui/src/pages/auth/ui/AuthForm.tsx
@@ -0,0 +1,34 @@
+import { Form } from 'antd';
+
+import FormData from '../model/FormData';
+
+interface AuthFormProps {
+  name: string;
+  isLoading: boolean;
+  onFinish: (data: FormData) => Promise<void>;
+  children: React.ReactNode;
+}
+
+export default function AuthForm({
+  name,
+  isLoading,
+  onFinish,
+  children,
+}: AuthFormProps) {
+  return (
+    <Form
+      name={name}
+      disabled={isLoading}
+      labelCol={{ span: 8 }}
+      wrapperCol={{ span: 16 }}
+      style={{
+        maxWidth: 500,
+      }}
+      initialValues={{ remember: true }}
+      onFinish={onFinish}
+      autoComplete="off"
+    >
+      {children}
+    </Form>
+  );
+}
