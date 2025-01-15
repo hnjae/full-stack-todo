d10ed58a46d5987d521ac57f7afb1762c152e649
KIM Hyunjae <42675338+hnjae@users.noreply.github.com>
2025-01-15 12:29:48 +0900

    feat(ui): apply updated MainHeader component across multiple pages

--- packages/ui/src/pages/signup.tsx
+++ packages/ui/src/pages/signup.tsx
@@ -1,6 +1,8 @@
 import { Button, Card, Form, Input, message } from 'antd';
+import { Layout } from 'antd';
 import { useState } from 'react';
 import env from 'src/shared/config/env';
+import MainHeader from 'src/shared/ui/MainHeader';
 
 // TODO: 이메일 인증? <2024-12-23>
 // TODO: card 를 적당히 화면 중앙에 뛰우기 <2024-12-24>
@@ -59,79 +61,85 @@ export default function SignupPage() {
   };
 
   return (
-    <Card title="Sign up" className="">
-      <Form
-        name="signup"
-        disabled={isLoading}
-        labelCol={{ span: 8 }}
-        wrapperCol={{ span: 16 }}
-        style={{ maxWidth: 400 }}
-        initialValues={{ remember: true }}
-        onFinish={onFinish}
-        // onFinishFailed={onFinishFailed}
-        autoComplete="off"
-      >
-        <Form.Item<FormData>
-          label="Email"
-          name="email"
-          rules={[
-            { required: true, message: 'Please input your email!' },
-            {
-              type: 'email',
-              message: 'The input is not valid E-mail',
-            },
-          ]}
+    <Layout>
+      <MainHeader />
+      <Card title="Sign up" className="">
+        <Form
+          name="signup"
+          disabled={isLoading}
+          labelCol={{ span: 8 }}
+          wrapperCol={{ span: 16 }}
+          style={{ maxWidth: 400 }}
+          initialValues={{ remember: true }}
+          onFinish={onFinish}
+          // onFinishFailed={onFinishFailed}
+          autoComplete="off"
         >
-          <Input placeholder="example@example.org" />
-        </Form.Item>
-
-        <Form.Item<FormData>
-          label="Password"
-          name="password"
-          rules={[
-            { required: true, message: 'Please input your password!' },
-            {
-              type: 'string',
-              min: 8,
-              max: 72,
-              message: 'The password must be between 8 and 72 characters long.',
-            },
-          ]}
-        >
-          <Input.Password placeholder="input password" />
-        </Form.Item>
-
-        <Form.Item
-          name="confirm"
-          label="Confirm Password"
-          dependencies={['password']}
-          hasFeedback
-          rules={[
-            {
-              required: true,
-              message: 'Please confirm your password!',
-            },
-            ({ getFieldValue }) => ({
-              validator(_, value) {
-                if (!value || getFieldValue('password') === value) {
-                  return Promise.resolve();
-                }
-                return Promise.reject(
-                  new Error('The new password that you entered do not match!'),
-                );
+          <Form.Item<FormData>
+            label="Email"
+            name="email"
+            rules={[
+              { required: true, message: 'Please input your email!' },
+              {
+                type: 'email',
+                message: 'The input is not valid E-mail',
               },
-            }),
-          ]}
-        >
-          <Input.Password />
-        </Form.Item>
+            ]}
+          >
+            <Input placeholder="example@example.org" />
+          </Form.Item>
 
-        <Form.Item label={null}>
-          <Button type="primary" htmlType="submit">
-            Submit
-          </Button>
-        </Form.Item>
-      </Form>
-    </Card>
+          <Form.Item<FormData>
+            label="Password"
+            name="password"
+            rules={[
+              { required: true, message: 'Please input your password!' },
+              {
+                type: 'string',
+                min: 8,
+                max: 72,
+                message:
+                  'The password must be between 8 and 72 characters long.',
+              },
+            ]}
+          >
+            <Input.Password placeholder="input password" />
+          </Form.Item>
+
+          <Form.Item
+            name="confirm"
+            label="Confirm Password"
+            dependencies={['password']}
+            hasFeedback
+            rules={[
+              {
+                required: true,
+                message: 'Please confirm your password!',
+              },
+              ({ getFieldValue }) => ({
+                validator(_, value) {
+                  if (!value || getFieldValue('password') === value) {
+                    return Promise.resolve();
+                  }
+                  return Promise.reject(
+                    new Error(
+                      'The new password that you entered do not match!',
+                    ),
+                  );
+                },
+              }),
+            ]}
+          >
+            <Input.Password />
+          </Form.Item>
+
+          <Form.Item label={null}>
+            <Button type="primary" htmlType="submit">
+              Submit
+            </Button>
+          </Form.Item>
+        </Form>
+      </Card>
+    </Layout>
   );
 }
