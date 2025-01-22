cb6ac0d3bb727007dd9082eb973c18cbf3739f05
KIM Hyunjae <42675338+hnjae@users.noreply.github.com>
2025-01-22 16:41:19 +0900

    feat(api): implement JwtAuthGuard

--- packages/api/src/app.controller.ts
+++ packages/api/src/app.controller.ts
@@ -1,6 +1,8 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { Request } from 'express';
 
 import { AppService } from './app.service';
+import { JwtAuthGuard } from './auth/jwt-auth.guard';
 
 @Controller()
 export class AppController {
@@ -8,6 +10,15 @@ export class AppController {
 
   @Get()
   getHello(): string {
+    return this.appService.getHello();
+  }
+
+  // TODO: remove this in production <2025-01-22>
+  @UseGuards(JwtAuthGuard)
+  @Get('auth-demo')
+  jwtAuthDemo(@Req() request: Request) {
+    // request.user 에 JwtAuthGuard 가 vallidate() 에서 반환한 값이 들어있음.
+    console.log(request);
     return this.appService.getHello();
   }
 }
