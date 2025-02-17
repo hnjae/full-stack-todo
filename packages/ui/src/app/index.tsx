b860262dac3eaeb8ecb37775e0e6ff6cfe5208d8
hnjae <hnjae@outlook.com>
2025-02-17 14:39:42 +0900

    feat(ui): integrate Redux store into the application

--- packages/ui/src/app/index.tsx
+++ packages/ui/src/app/index.tsx
@@ -4,11 +4,15 @@ import '@fontsource/material-icons-outlined';
 import { RouterProvider } from '@tanstack/react-router';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 
 import router from './router';
+import { store } from './store';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>,
 );
---
+++ packages/ui/src/app/store.ts
@@ -0,0 +1,11 @@
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+
+const rootReducer = combineReducers({});
+
+export const store = configureStore({
+  reducer: rootReducer,
+});
+
+export type AppStore = typeof store;
+export type RootState = ReturnType<AppStore['getState']>;
+export type AppDispatch = AppStore['dispatch'];
