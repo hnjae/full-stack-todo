26a1dc4189b731af986b16c538476a39cb6a0b61
KIM Hyunjae <hnjae@outlook.com>
2025-03-10 22:42:14 +0900

    feat(api): add refresh token validation method

--- packages/api/src/auth/auth.service.ts
+++ packages/api/src/auth/auth.service.ts
@@ -4,6 +4,7 @@ import * as bcrypt from 'bcrypt';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateUserDto, LoginUserDto, UserDto } from 'src/users/users.dto';
 import { UsersService } from 'src/users/users.service';
+import { z } from 'zod';
 
 import { JwtPayloadData } from './jwt.strategy';
 import { TokenRequestDto } from './token-request.dto';
@@ -12,6 +13,17 @@ import { TokenRequestDto } from './token-request.dto';
 // TODO: test 를 위해 expiresIn 을 길게 잡음. 나중에 수정할 것 <2024-12-12>
 const ACCESS_TOKEN_EXPIRES_IN = 86400; // 단위 seconds
 const REFRESH_TOKEN_EXPIRES_IN = 7 * 24 * 60 * 60; // 7 days
+
+const refreshTokenPayloadSchema = z
+  .object({
+    sub: z.string(),
+    jti: z.string(),
+    exp: z.number(),
+    iat: z.number(),
+  })
+  .strict();
+
+type RefreshTokenPayload = z.infer<typeof refreshTokenPayloadSchema>;
 
 @Injectable()
 export class AuthService {
@@ -25,6 +37,7 @@ export class AuthService {
     return this.usersService.create(userDto);
   }
 
+  // TODO: rename this <2025-03-10>
   async validate(userDto: LoginUserDto) {
     const user = await this.usersService.getByEmail(userDto.email, {
       includeSensitive: true,
@@ -41,6 +54,55 @@ export class AuthService {
     return true;
   }
 
+  /**
+   * Returns a token payload or false if the token is invalid.
+   *
+   * @remarks:
+   * To support refresh token rotation, the following side effects are performed:
+   * - If the refresh token is invalid, revoke all refresh tokens for the user. (TODO)
+   * - Since the current refresh token has been used, revoke this refresh token. (TODO)
+   */
+  async validateAndRotateRefreshToken(token: string): Promise<UserDto | false> {
+    try {
+      const payload = await this.jwtService.verifyAsync(token);
+
+      const payloadParsed = refreshTokenPayloadSchema.safeParse(payload);
+
+      if (!payloadParsed.success) {
+        console.log('Invalid refresh token payload: ', payload.data);
+        return false;
+      }
+
+      const refreshTokenRecord =
+        await this.prismaService.refreshToken.findUnique({
+          where: { id: payloadParsed.data.jti },
+          include: { user: true },
+        });
+
+      if (
+        refreshTokenRecord == null ||
+        refreshTokenRecord.userId !== payloadParsed.data.sub // 이거 검증할 필요가 있나?
+      ) {
+        // TODO: revoke all refresh token <2025-03-10>
+        console.log('Invalid refresh token');
+        return false;
+      }
+
+      // Check if the token has expired
+      const now = new Date();
+      if (refreshTokenRecord.expiresAt < now) {
+        console.log('Refresh token has expired');
+        return false;
+      }
+
+      // TODO: revoke old refresh token <2025-03-10>
+      return refreshTokenRecord.user;
+    } catch (JsonWebTokenError) {
+      console.log('Invalid refresh token (JWT error)');
+      return false;
+    }
+  }
+
   async issueRefreshToken(userId: string) {
     const iat = Math.floor(Date.now() / 1000);
     const exp = iat + REFRESH_TOKEN_EXPIRES_IN;
@@ -53,7 +115,7 @@ export class AuthService {
       },
     });
 
-    const payload = {
+    const payload: RefreshTokenPayload = {
       sub: userId,
       jti: registered.id,
       exp,
