e701565b2047360dbd3764d1a701221596b27f3f
KIM Hyunjae <42675338+hnjae@users.noreply.github.com>
2025-03-14 19:49:20 +0900

    feat(ui): renew access token when App initialize
    If a refresh token is available, attempt to retrieve an access
    token using it on app initialization.

--- packages/ui/src/app/index.tsx
+++ packages/ui/src/app/index.tsx
@@ -2,17 +2,59 @@ import './style.css';
 import '@fontsource/material-icons-outlined';
 
 import { RouterProvider } from '@tanstack/react-router';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
+import { getTokens, refreshTokenService } from 'src/shared/lib';
+import { setAccessToken } from 'src/shared/model';
 
 import router from './router';
 import { store } from './store';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <RouterProvider router={router} />
-    </Provider>
-  </React.StrictMode>,
-);
+const App = function () {
+  const [authInitialized, setAuthInitialized] = useState<boolean | null>(null);
+
+  useEffect(() => {
+    const initAccessToken = async function () {
+      try {
+        const refreshToken = refreshTokenService.get();
+
+        if (refreshToken == null) {
+          throw new Error('No refresh token found');
+        }
+
+        const formParams = new URLSearchParams();
+        formParams.append('grant_type', 'refresh_token');
+        formParams.append('refresh_token', refreshToken);
+
+        const { accessToken, refreshToken: newRefreshToken } =
+          await getTokens(formParams);
+
+        refreshTokenService.set(newRefreshToken);
+        store.dispatch(setAccessToken(accessToken));
+        console.log('Successfully got access token.');
+        setAuthInitialized(true);
+      } catch (error) {
+        console.log('Failed to get accesss token:', error);
+        refreshTokenService.remove();
+        setAuthInitialized(false);
+      }
+    };
+
+    initAccessToken();
+  }, []);
+
+  if (authInitialized == null) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <React.StrictMode>
+      <Provider store={store}>
+        <RouterProvider router={router} />
+      </Provider>
+    </React.StrictMode>
+  );
+};
+
+ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
