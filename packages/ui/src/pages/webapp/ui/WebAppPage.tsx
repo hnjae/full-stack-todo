0cdc3017c784eb7a5e702a66d19eb0067c55a326
KIM Hyunjae <hnjae@outlook.com>
2025-03-31 16:17:02 +0900

    feat(ui): add sidebar with todo lists

--- packages/ui/src/pages/webapp/ui/WebAppPage.tsx
+++ packages/ui/src/pages/webapp/ui/WebAppPage.tsx
@@ -1,85 +1,132 @@
 // NOTE: This page assumes that the user is logged in when loaded.
 
-import { Layout, theme } from 'antd';
+import { PlusOutlined } from '@ant-design/icons';
+import { Divider, Input, Layout, theme, Tree, TreeDataNode } from 'antd';
+import { useMemo, useState } from 'react';
 import { useGetTodoListsQuery } from 'src/entities/todo-list';
 import { useGetUserInfoQuery } from 'src/entities/user';
 import { MainHeader } from 'src/widgets/header';
 
-const { Content } = Layout;
+const { Content, Sider } = Layout;
 
 export default function WebAppPage() {
   const { token } = theme.useToken();
 
   const { data: user, isLoading, isFetching, isError } = useGetUserInfoQuery();
-  const { data: todoLists } = useGetTodoListsQuery();
+  // TODO: loading 중일때는 화면에 표기 <2025-03-31>
+  const { data: todoLists, isTodoListLoading } = useGetTodoListsQuery();
+
+  const todoListTreeData: TreeDataNode[] | undefined = useMemo(() => {
+    return todoLists
+      ?.slice()
+      .sort((a, b) => a.order - b.order)
+      .map((todoList) => {
+        return {
+          key: todoList.id,
+          title: todoList.name,
+          disableCheckbox: true,
+        };
+      });
+  }, [todoLists]);
+
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
 
   return (
     <Layout
       style={{
-        height: '100dvh',
+        height: '97dvh',
       }}
     >
       <MainHeader />
-      <Content
-        style={{
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
-          paddingBottom: '20vh',
-          background: `color-mix(in srgb, ${token.colorBgLayout}, black 2%)`,
-        }}
-      >
-        <div>
-          <p>Todo-app should be implemented here</p>
-          <ul>
-            <li>
-              {isFetching ? 'Fetching User Info...' : <div>not fetching</div>}
-            </li>
-            <li>
-              {isLoading ? 'Loading User Info...' : <div>not loading</div>}
-            </li>
-            <li>
-              {isError ? 'Error loading user info...' : <div>not error</div>}
-            </li>
-          </ul>
-        </div>
-        <p>
-          {user != null ? (
-            <div>
-              <br />
-              User Id: {user.id}
-              <br />
-              User Email: {user.email}
-              <br />
-              User createdAt: {user.createdAt}
+      <Layout>
+        <Sider
+          collapsed={false}
+          style={{
+            background: colorBgContainer,
+          }}
+        >
+          <div
+            style={{
+              height: '100%',
+              display: 'flex',
+              flexDirection: 'column',
+            }}
+          >
+            <div
+              style={{
+                overflowY: 'auto',
+                overflowX: 'hidden',
+              }}
+            >
+              <Tree
+                className="draggable-tree"
+                draggable
+                blockNode
+                defaultSelectedKeys={
+                  todoListTreeData?.[0] != null
+                    ? [todoListTreeData[0].key]
+                    : undefined
+                }
+                // defaultExpandedKeys={expandedKeys}
+                // onDragEnter={onDragEnter}
+                // onDrop={onDrop}
+                treeData={todoListTreeData}
+              />
             </div>
-          ) : null}
-        </p>
-        <br />
-        <p>
-          {todoLists != null ? (
-            <div>
-              <br />
-              <ul>
-                {todoLists.map((todoList) => (
-                  <li key={todoList.id}>
-                    <div>
-                      Todo List Id: {todoList.id}
-                      <br />
-                      Todo List Name: {todoList.name}
-                      <br />
-                      Todo List Order: {todoList.order}
-                      <br />
-                    </div>
-                  </li>
-                ))}
-              </ul>
+
+            <div style={{ flexShrink: 0 }}>
+              <Divider />
+
+              <Input
+                addonBefore={<PlusOutlined />}
+                placeholder="New List"
+                variant="filled"
+                style={{
+                  paddingRight: '8px',
+                }}
+              />
             </div>
-          ) : (
-            <div>no todo list</div>
-          )}
-        </p>
-      </Content>
+          </div>
+        </Sider>
+        <Content
+          style={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            paddingBottom: '20vh',
+            background: `color-mix(in srgb, ${token.colorBgLayout}, black 2%)`,
+          }}
+        >
+          <div>
+            <p>Todo-app should be implemented here</p>
+            <ul>
+              <li>
+                {isFetching ? 'Fetching User Info...' : <div>not fetching</div>}
+              </li>
+              <li>
+                {isLoading ? 'Loading User Info...' : <div>not loading</div>}
+              </li>
+              <li>
+                {isError ? 'Error loading user info...' : <div>not error</div>}
+              </li>
+            </ul>
+          </div>
+          <div>
+            {user != null ? (
+              <div>
+                <br />
+                User Id: {user.id}
+                <br />
+                User Email: {user.email}
+                <br />
+                User createdAt: {user.createdAt}
+              </div>
+            ) : null}
+          </div>
+        </Content>
+      </Layout>
     </Layout>
   );
 }
