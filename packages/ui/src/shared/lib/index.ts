1df9d2ab8e2c12d7596b82a912a3d1cc608524b6
KIM Hyunjae <hnjae@outlook.com>
2025-05-12 18:54:25 +0900

    refactor(ui): extract order calculation logic to shared

--- packages/ui/src/shared/lib/index.ts
+++ packages/ui/src/shared/lib/index.ts
@@ -1,3 +1,7 @@
 export { AuthenticationError, default as getTokens } from './getTokens';
-export { balanceItems, ORDER_SPACING } from './orderedListUtils';
+export {
+  balanceItems,
+  getNextOrderAndBalancedItems,
+  ORDER_SPACING,
+} from './orderedListUtils';
 export { default as refreshTokenService } from './refreshTokenService';
--- packages/ui/src/shared/lib/orderedListUtils.ts
+++ packages/ui/src/shared/lib/orderedListUtils.ts
@@ -1,6 +1,12 @@
+import { MAX_INTEGER } from '../config';
+
 const ORDER_SPACING = 65536; // 2^16
 
-const balanceItems = function <T extends { order: number }>(
+interface OrderedItem {
+  order: number;
+}
+
+const balanceItems = function <T extends OrderedItem>(
   sortedItems: readonly T[],
 ): T[] {
   if (sortedItems.length === 0) {
@@ -14,4 +20,26 @@ const balanceItems = function <T extends { order: number }>(
   });
 };
 
-export { balanceItems, ORDER_SPACING };
+const getNextOrderAndBalancedItems = function <T extends OrderedItem>(
+  items: readonly T[] | null | undefined,
+): { balancedItems: T[] | null; order: number } {
+  let order = 0;
+
+  if (items == null || items.length === 0) {
+    return { balancedItems: null, order };
+  }
+
+  const lastOrder = items[items.length - 1].order;
+
+  if (lastOrder > MAX_INTEGER - ORDER_SPACING) {
+    const balancedItems = balanceItems(items);
+    order = balancedItems[balancedItems.length - 1].order;
+
+    return { balancedItems, order };
+  }
+
+  order = lastOrder + ORDER_SPACING;
+  return { balancedItems: null, order };
+};
+
+export { balanceItems, getNextOrderAndBalancedItems, ORDER_SPACING };
