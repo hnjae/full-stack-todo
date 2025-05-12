c26393e122c1bb1bb61209a2a89fc264f73a7329
KIM Hyunjae <42675338+hnjae@users.noreply.github.com>
2025-05-12 17:19:42 +0900

    refactor(ui): move balanceItems and ORDER_SPACING to shared

--- packages/ui/src/shared/lib/index.ts
+++ packages/ui/src/shared/lib/index.ts
@@ -1,2 +1,3 @@
 export { AuthenticationError, default as getTokens } from './getTokens';
+export { balanceItems, ORDER_SPACING } from './orderedListUtils';
 export { default as refreshTokenService } from './refreshTokenService';
