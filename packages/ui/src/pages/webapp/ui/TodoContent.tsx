b5fc216677d789b69db36a193d97d9d488d42c5b
KIM Hyunjae <42675338+hnjae@users.noreply.github.com>
2025-05-12 13:32:57 +0900

    feat(ui): implement todo content layout

--- packages/ui/src/pages/webapp/ui/TodoContent.tsx
+++ packages/ui/src/pages/webapp/ui/TodoContent.tsx
@@ -1,4 +1,6 @@
-import { Layout, theme } from 'antd';
+import { PlusOutlined } from '@ant-design/icons';
+import { Card, Input, Layout, theme } from 'antd';
+
 const { Content } = Layout;
 
 export default function TodoContent({
@@ -7,19 +9,42 @@ export default function TodoContent({
   selectedTodoListId: string | null;
 }) {
   const { token } = theme.useToken();
+  const card = (
+    <div className="m-2">
+      <Card className="h-fit" variant="borderless">
+        Card content
+      </Card>
+    </div>
+  );
 
   return (
     <Content
-      className="flex items-center justify-center pb-[20vh]"
+      className="flex flex-col p-4"
       style={{
         background: `color-mix(in srgb, ${token.colorBgLayout}, black 2%)`,
       }}
     >
-      {selectedTodoListId == null && (
-        <div>
+      {selectedTodoListId == null ? (
+        <div className="flex justify-center items-center h-full">
           No Todo list has been selected. Please choose a list from the left or
           create a new one.
         </div>
+      ) : (
+        <>
+          <Input
+            addonBefore={<PlusOutlined />}
+            size="large"
+            placeholder="New Todo"
+            className="mb-2"
+          />
+          <div className="overflow-y-auto">
+            {card}
+            {card}
+            {card}
+            {card}
+            {card}
+          </div>
+        </>
       )}
     </Content>
   );
