38253718407f328242be5901aa3e619edc024a20
KIM Hyunjae <hnjae@outlook.com>
2025-05-13 16:04:50 +0900

    feat(ui): add checkbox to update todo completion

--- packages/ui/src/features/todo/index.ts
+++ packages/ui/src/features/todo/index.ts
@@ -3,3 +3,4 @@ export type { ModalState as RenameTodoModalState } from './ui/DeleteTodoModal';
 export { default as DeleteTodoModal } from './ui/DeleteTodoModal';
 export { default as RenameTodoModal } from './ui/RenameTodoModal';
 export { default as useHandleAddingTodo } from './ui/useHandleAddingTodo';
+export { default as useUpdateTodosCompletion } from './ui/useUpdateTodosCompletion';
